e1481c6bb313fc77cbda9b62f1e6e40550378c83
Yuri Bom Ribeiro <yuribomr@gmail.com>
2022-01-03 16:53:47 -0300

    Adicionando Contador

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber
+}
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber
+}
